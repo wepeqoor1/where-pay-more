2a711a5843028befd6638e99e3ddad01f8c868c2
Egor Kochergin <46388832+wepeqoor1@users.noreply.github.com>
2022-07-25 23:03:15 +0300

    Search statistics popular programming languages

--- count_vacancies.py
+++ count_vacancies.py
@@ -1,6 +1,28 @@
 import requests
+import statistics
 
-from utils import get_vacancies
+
+def predict_rub_salary(vacancy: dict) -> float|None:
+    salary = vacancy.get('salary')
+    if not salary:
+        return None
+    if not salary['from']:
+        return salary['to'] * 0.8
+    if not salary['to']:
+        return salary['from'] * 1.2
+    return (salary['to'] - salary['from']) / 2 + salary['from']
+
+
+def get_vacancies(url: str, text: str) -> dict:
+    params = {
+        "professional_role": 96,  # Specialization programmer id
+        "area": 1,  # Moscow
+        "text": text  # Searched text
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    
+    return response.json()
 
 
 if __name__ == '__main__':
@@ -17,7 +39,8 @@ if __name__ == '__main__':
     ]
 
     url = 'https://api.hh.ru/vacancies'
-    programmer_languages_count = {}
+
+    one_page_salary_statistics = {}
     
     try:
         for programmer_language in programmer_languages:
@@ -26,10 +49,18 @@ if __name__ == '__main__':
                 url = url,
                 text=text,
                 )
-            vacancies_count = vacancies['found']
-            programmer_languages_count[programmer_language] = vacancies_count
+            average_salaries = [predict_rub_salary(vacancy=vacancy) for vacancy in vacancies['items']]
+            actual_average_salaries = list(filter(None, average_salaries))
+            
+            language_salary_statistic = {
+                    programmer_language: {
+                        "vacancies_found": vacancies['found'],
+                        "vacancies_processed": len(actual_average_salaries),
+                        "average_salary": int(statistics.mean(actual_average_salaries)),
+                    }
+                }
+            one_page_salary_statistics.update(language_salary_statistic)
             
     except requests.HTTPError as error:
         print('Что то не получилось')
-        
-    print(programmer_languages_count)
+    
