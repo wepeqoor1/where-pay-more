08f54e5ce0f5e83455f2eb7fa96f3d7c3f3e9d60
Egor Kochergin <46388832+wepeqoor1@users.noreply.github.com>
2022-08-23 01:20:04 +0300

    Fix variable names

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 from dotenv import load_dotenv
 from tabulate import tabulate
+from functools import partial
 
 from table_statistic_tools import get_language_statistic
 from superjob_salary import get_language_vacancies_sj, predict_rub_salary_sj
@@ -8,6 +10,8 @@ from headhunter_salary import get_language_vacancies_hh, predict_rub_salary_hh
 
 if __name__ == '__main__':
     load_dotenv()
+    api_key = os.getenv('SUPER_JOB_API_KEY')
+    
     languages = [
         'JavaScript',
         'Java',
@@ -19,23 +23,23 @@ if __name__ == '__main__':
         'Go',
     ]
     
-    table_statistic_sj = []
     table_statistic_hh = []
+    table_statistic_sj = []
     
     for language in languages:
-        language_vacancies = list(get_language_vacancies_sj(language))
-        all_expected_salaries = list(filter(lambda x: x, map(predict_rub_salary_sj, language_vacancies)))
-        table: dict = get_language_statistic(language_vacancies, all_expected_salaries, language)
-        table_statistic_sj.append(table)
+        language_vacancies_hh = list(get_language_vacancies_hh(language))
+        all_expected_salaries_hh = list(filter(lambda x: x, map(predict_rub_salary_hh, language_vacancies_hh)))
+        table_hh: dict = get_language_statistic(language_vacancies_hh, all_expected_salaries_hh, language)
+        table_statistic_hh.append(table_hh)
         
-    for language in languages:
-        language_vacancies = list(get_language_vacancies_hh(language))
-        all_expected_salaries = list(filter(lambda x: x, map(predict_rub_salary_hh, language_vacancies)))
-        table: dict = get_language_statistic(language_vacancies, all_expected_salaries, language)
-        table_statistic_hh.append(table)
+        language_vacancies_sj = list(get_language_vacancies_sj(language, api_key))
+        all_expected_salaries_sj = list(filter(lambda x: x, map(predict_rub_salary_sj, language_vacancies_sj)))
+        table_sj: dict = get_language_statistic(language_vacancies_sj, all_expected_salaries_sj, language)
+        table_statistic_sj.append(table_sj)
         
     print('SuperJob')
     print(tabulate(table_statistic_sj, headers='keys', tablefmt="grid"))
         
     print('HeadHunter')
     print(tabulate(table_statistic_hh, headers='keys', tablefmt="grid"))
+    
